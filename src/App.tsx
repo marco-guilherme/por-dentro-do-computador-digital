5930efdf9a8ef713a449ab6aa5c5f36aa3f306bf
Marco Guilherme <marco.pereira@xmobots.com.br>
2022-09-11 22:08:59 -0300

    [PDCD-5] Configurar página inicial do site
    Adição de cabeçalho básico para a página inicial.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { About } from './components/About';
 import { Home } from './components/Home';
+import './styles/App.css';
 
-const App = () => {
+const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,28 @@
-export const Home = () => {
-  return <div>Home</div>;
+import React from 'react';
+import { Layout, Menu } from 'antd';
+
+const { Header } = Layout;
+
+export const Home = (): JSX.Element => {
+  return (
+    <div>
+      <Layout>
+        <Header>
+          <div className="header-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['2']}
+            items={new Array(5).fill(null).map((_, index) => {
+              const key = index + 1;
+              return {
+                key,
+                label: `nav ${key}`,
+              };
+            })}
+          />
+        </Header>
+      </Layout>
+    </div>
+  );
 };
