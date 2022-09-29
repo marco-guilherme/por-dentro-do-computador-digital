97192daf03e3a8b27364afc29da0043ea83d295b
Marco Pereira <marco.pereira@xmobots.com.br>
2022-09-28 22:52:49 -0300

    [PDCD-6] Criar elemento aside para segmentar o conteúdo
    Divisão da página em duas colunas.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,38 +1,47 @@
+import { Tree } from 'antd';
+
 export const Content = (): JSX.Element => {
+  const treeData = [
+    {
+      title: 'Eletrônica Digital',
+      key: 'de',
+      children: [
+        { title: 'Portas Lógicas', key: 'lg' },
+        { title: 'Registradores', key: 'reg' },
+      ],
+    },
+    {
+      title: 'Linguagem Assembly x16',
+      key: 'asm',
+      children: [{ title: 'Instruções', key: 'inst' }],
+    },
+  ];
+
   return (
-    <div>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
-      <h1>Content</h1>
+    <div className="grid-container">
+      <main className="main-content">
+        <h1>Portas Lógicas</h1>
+        <p>A porta lógica AND bla bla bla</p>
+        <iframe
+          src="https://circuitverse.org/simulator/embed/hello-world-02cbd5bf-29e0-4c20-a2a7-947c1639f114?theme=default&display_title=false&clock_time=true&fullscreen=true&zoom_in_out=true"
+          name="myiframe"
+          id="projectPreview"
+          scrolling="no"
+          frameBorder="1"
+          height="500"
+          width="500"
+          allowFullScreen
+        ></iframe>
+      </main>
+      <aside className="aside-content">
+        <h2>Conteúdo do curso</h2>
+        <Tree
+          className="tree-content"
+          treeData={treeData}
+          defaultExpandAll
+          onClick={e => console.log(e)}
+        />
+      </aside>
     </div>
   );
 };
