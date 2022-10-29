cf922017a94bed6dfef9cef767367ee950c37214
Marco Pereira <marco.pereira@xmobots.com.br>
2022-10-29 10:59:27 -0300

    [PDCD-7] Escrever as páginas básicas
    Descrição báscias sobre a porta AND e melhoria no texto do "Sobre".

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,17 @@
 export const About = (): JSX.Element => {
   return (
-    <div>
-      <h1>Sobre</h1>
-      <p>
-        Trabalho de Conclusão de Curso para obtenção do título de Graduação em
-        Ciência da Computação, apresentado à Universidade Paulista – UNIP
-        (Araraquara, 2022)
-      </p>
+    <div className="site-content-wrapper">
+      <h1>Sobre Este Site</h1>
+      <div className="site-content">
+        <p>
+          Este site foi desenvolvido por Marco Guilherme como Trabalho de
+          Conclusão de Curso (TCC) para obtenção do título de Graduação em
+          Ciência da Computação (bacharelado). Ele será apresentado à
+          Universidade Paulista – UNIP.
+        </p>
+        <br />
+        <p style={{ textAlign: 'right' }}>Araraquara, 2022</p>
+      </div>
     </div>
   );
 };
--- src/pages/AndGate.tsx
+++ src/pages/AndGate.tsx
@@ -1,3 +1,32 @@
+import andGateAnsi from '../images/and-gate-ansi.png';
+import andGateIec from '../images/and-gate-iec.png';
+
 export const AndGate = (): JSX.Element => {
-  return <div>AndGate</div>;
+  return (
+    <div className="site-content-wrapper">
+      <h1>Porta Lógica AND</h1>
+      <div className="site-content">
+        <p>
+          Na porta AND a saída só será um (nível lógico alto), se todas as suas
+          entradas forem iguais a um. Caso contrário, teremos zero na saída.
+          Simbologia no padrão ANSI:
+        </p>
+        <div className="site-image">
+          <img
+            src={andGateAnsi}
+            className="and-gate-symbol-image"
+            alt="Simbologia da porta lógica AND no padrão ANSI"
+          />
+        </div>
+        <p>Simbologia no padrão IEC:</p>
+        <div className="site-image">
+          <img
+            src={andGateIec}
+            className="and-gate-symbol-image"
+            alt="Simbologia da porta lógica AND no padrão IEC"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
