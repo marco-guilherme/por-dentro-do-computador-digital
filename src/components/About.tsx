306fdaa270b72a4a289c845c7343bede2753ce1b
Marco Pereira <marco.pereira@xmobots.com.br>
2022-11-04 19:38:12 -0300

    [PDCD-10] Escrever página sobre a tabela ASCII
    Alterações na página do "sobre".

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import unipAraraquara from '../images/unip-araraquara.jpg';
+
 export const About = (): JSX.Element => {
   return (
     <div className="site-content-wrapper">
@@ -5,12 +7,19 @@ export const About = (): JSX.Element => {
       <div className="site-content">
         <p>
           Este site foi desenvolvido por Marco Guilherme como Trabalho de
-          Conclusão de Curso (TCC) para obtenção do título de Graduação em
+          Conclusão de Curso (TCC) para obtenção do título de graduação em
           Ciência da Computação (bacharelado). Ele será apresentado à
-          Universidade Paulista – UNIP.
+          Universidade Paulista – UNIP, campus Araraquara.
         </p>
         <br />
-        <p style={{ textAlign: 'right' }}>Araraquara, 2022</p>
+        <p style={{ textAlign: 'right' }}>São Carlos, 2022</p>
+        <div className="site-image">
+          <img
+            src={unipAraraquara}
+            alt="Universidade Paulista - UNIP, campus Araraquara"
+            style={{ width: '60vw' }}
+          />
+        </div>
       </div>
     </div>
   );
