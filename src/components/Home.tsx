918f9d7f7c266a4dc3a26161b29c3ddd5e2f97c4
Marco Pereira <marco.pereira@xmobots.com.br>
2022-11-06 00:06:40 -0300

    [PDCD-11] Desenvolver a página inicial
    Adição de GIF na página inicial, fonte retrô para o título e grade.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,56 @@
+import transistorNpn from '../images/transistor-npn.png';
+import srClockedFlipFlop from '../images/sr-clocked-flip-flop.png';
+
 export const Home = (): JSX.Element => {
-  return <div>Home</div>;
+  return (
+    <div className="homepage">
+      <div className="first-fraction flex-center-column">
+        <h1>
+          <span>Por Dentro do Computador Digital</span>
+        </h1>
+        <h3>
+          <span>
+            Website Interativo Para Ensino de Tópicos de Eletrônica Digital e
+            Linguagem Assembly
+          </span>
+        </h3>
+      </div>
+      <h2>Por Que Aprender Eletrônica Digital?</h2>
+      <div className="grid-container-homepage">
+        <div className="grid-container-homepage-item flex-center-column">
+          <img
+            src={srClockedFlipFlop}
+            alt="Flip-flop SR ativo por clock"
+            className="grid-container-homepage-item-icon"
+          />
+          <h4>Análise de circuitos eletrônicos</h4>
+        </div>
+        <div className="grid-container-homepage-item flex-center-column">
+          <img
+            src={transistorNpn}
+            alt="Simbologia do transistor NPN"
+            className="grid-container-homepage-item-icon"
+          />
+          <h4>Construção de circuitos</h4>
+        </div>
+        <div className="grid-container-homepage-item flex-center-column">
+          <img
+            src={transistorNpn}
+            alt="Simbologia do transistor NPN"
+            className="grid-container-homepage-item-icon"
+          />
+          <h4>Manutenção em PCBs</h4>
+        </div>
+        <div className="grid-container-homepage-item flex-center-column">
+          <img
+            src={transistorNpn}
+            alt="Simbologia do transistor NPN"
+            className="grid-container-homepage-item-icon"
+          />
+          <h4>Desenvolvimento de projetos de automação</h4>
+        </div>
+      </div>
+      <h2>Por Que Aprender Assembly?</h2>
+    </div>
+  );
 };
