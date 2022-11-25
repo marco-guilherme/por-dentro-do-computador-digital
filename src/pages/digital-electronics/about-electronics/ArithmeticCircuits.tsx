1a1e4704745fa360eecbd6251309ea771d0b9ce6
Marco Pereira <marco.pereira@xmobots.com.br>
2022-11-25 15:17:22 -0300

    [PDCD-29] Desenvolver a página sobre circuitos aritméticos
    Introdução aos circuitos aritméticos.

--- src/pages/digital-electronics/about-electronics/ArithmeticCircuits.tsx
+++ src/pages/digital-electronics/about-electronics/ArithmeticCircuits.tsx
@@ -6,7 +6,26 @@ export const ArithmeticCircuits = (): JSX.Element => {
       <h1>Circuitos Aritméticos</h1>
 
       <div className="site-content">
-        <p></p>
+        <p>
+          Um <span className="bold-text">circuito aritmético</span> é um tipo de
+          circuito combinacional que executa operações de adição, subtração,
+          multiplicação, divisão, operações lógicas ou qualquer outra função que
+          possa ser implementada em um circuito combinacional. Os circuitos
+          aritméticos são chamados de <span className="bold-text">ALU</span> (
+          <span className="italic-text">Arithmetic Logic Unit</span> ou Unidade
+          Lógica e Aritmética em português).
+        </p>
+
+        <p>
+          A quantidade de bits de um circuito aritmético é igual a quantidade de
+          saídas que esse circuito possui.
+        </p>
+
+        <p>
+          Para realizar operações de adição, utilizamos os{' '}
+          <span className="bold-text">circuitos somadores</span>. Para operações
+          de subtração, os <span className="bold-text">ssubtratores</span>.
+        </p>
 
         <br />
 
