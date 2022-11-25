91c0582f950bc58d7b76040a754c1d5942b36b68
Marco Pereira <marco.pereira@xmobots.com.br>
2022-11-25 15:08:22 -0300

    [PDCD-29] Desenvolver a página sobre circuitos aritméticos
    Correção nas chaves da árvore do drawer.

---
+++ src/pages/digital-electronics/about-electronics/ArithmeticCircuits.tsx
@@ -0,0 +1,24 @@
+import { PageInformation } from '../../../components/PageInformation';
+
+export const ArithmeticCircuits = (): JSX.Element => {
+  return (
+    <div className="site-content-wrapper">
+      <h1>Circuitos Aritméticos</h1>
+
+      <div className="site-content">
+        <p></p>
+
+        <br />
+
+        <hr />
+
+        <br />
+
+        <PageInformation
+          lastUpdateDate="25/11/2022"
+          originalPublicationDate="25/11/2022"
+        />
+      </div>
+    </div>
+  );
+};
